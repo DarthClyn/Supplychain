3da3dc166f7abc02e30eb5f21997ca5bd924a742
DarthNexus <harshitsaini2002@gmail.com>
2024-04-08 13:57:29 +0530

    .

---
+++ Images/index.js
@@ -0,0 +1,34 @@
+import shipCount from "./5.png";
+import getShipment from "./6.jpg";
+import compShipment from "./1.jpg";
+import startShipment from "./2.jpg";
+import userProfile from "./3.jpg";
+import send from "./4.jpg";
+
+import avatar from "./creator9.png";
+import logo from "./logo.png"
+
+import f from "./slides/5.jpg";
+import s from "./slides/6.jpg";
+import o from "./slides/1.jpg";
+import t from "./slides/2.jpg";
+import th from "./slides/3.jpg";
+import fo from "./slides/4.jpg";
+
+
+export default {
+  shipCount,
+  getShipment,
+  compShipment,
+  startShipment,
+  userProfile,
+  send,
+  avatar,
+  logo,
+  f,
+  s,
+  o,
+  t,
+  th,
+  fo,
+};
